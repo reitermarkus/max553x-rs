40577655fb54d7048826826a9ba4ec83727a14d6
Markus Reiter <me@reitermark.us>
2022-05-18 03:30:04 +0200

    Combine macros.

--- src/lib.rs
+++ src/lib.rs
@@ -62,116 +62,6 @@ macro_rules! impl_into_mode {
   };
 }
 
-macro_rules! impl_max {
-  ($(#[$attr:meta]),* $max_ty:ident) => {
-    $(
-      #[$attr]
-    )*
-    #[derive(Debug)]
-    pub struct $max_ty<W, MODE> {
-      writer: W,
-      _mode: PhantomData<MODE>,
-    }
-
-    impl<W, MODE> $max_ty<W, MODE> {
-      /// Create a new DAC with the given writer.
-      pub fn new(writer: W) -> $max_ty<W, Shutdown> {
-        $max_ty { writer, _mode: PhantomData }
-      }
-
-      /// Release the contained writer.
-      pub fn release(self) -> W {
-        self.writer
-      }
-    }
-
-    impl<W, MODE> $max_ty<W, MODE>
-    where
-      W: Write<u8>
-    {
-      /// Load input register A from shift register.
-      #[inline]
-      pub fn input_a(&mut self, value: u16) -> Result<(), W::Error> {
-        self.writer.write(&command_bytes(0b0001, value))
-      }
-
-      /// Load input register B from shift register.
-      #[inline]
-      pub fn input_b(&mut self, value: u16) -> Result<(), W::Error> {
-        self.writer.write(&command_bytes(0b0010, value))
-      }
-
-      impl_into_mode!("normal operation", $max_ty, Normal,   into_normal,   0b1101);
-      impl_into_mode!("shutdown",         $max_ty, Shutdown, into_shutdown, 0b1110);
-    }
-  }
-}
-
-impl_max!(
-  /// Struct representing a MAX5532 DAC.
-  Max5532
-);
-impl_max!(
-  /// Struct representing a MAX5533 DAC.
-  Max5533
-);
-impl_max!(
-  /// Struct representing a MAX5534 DAC.
-  Max5534
-);
-impl_max!(
-  /// Struct representing a MAX5535 DAC.
-  Max5535
-);
-
-macro_rules! impl_normal {
-  ($max_ty:ident) => {
-    impl<W> $max_ty<W, Normal>
-    where
-      W: Write<u8>
-    {
-      /// Load DAC registers A and B from respective input registers
-      /// and update respective DAC outputs.
-      #[inline]
-      pub fn dac_ab(&mut self, value: u16) -> Result<(), W::Error> {
-        self.writer.write(&command_bytes(0b1000, value))
-      }
-
-      /// Load input and DAC register A from shift register A and
-      /// load DAC register B from input register B.
-      pub fn input_a_dac_ab(&mut self, value: u16) -> Result<(), W::Error> {
-        self.writer.write(&command_bytes(0b1001, value))
-      }
-
-      /// Load input and DAC register B from shift register B and
-      /// load DAC register A from input register A.
-      pub fn input_b_dac_ab(&mut self, value: u16) -> Result<(), W::Error> {
-        self.writer.write(&command_bytes(0b1010, value))
-      }
-
-      impl_into_mode!("standby", $max_ty, Standby, into_standby, 0b1100);
-    }
-  }
-}
-
-impl_normal!(Max5532);
-impl_normal!(Max5533);
-impl_normal!(Max5534);
-impl_normal!(Max5535);
-
-/// Internal voltage reference for MAX5533/MAX5535.
-#[derive(Debug, Clone, Copy)]
-pub enum Vref {
-  /// 1.214 V
-  M1214 = 0b00,
-  /// 1.940 V
-  M1940 = 0b01,
-  /// 2.425 V
-  M2425 = 0b10,
-  /// 3.885 V
-  M3885 = 0b11,
-}
-
 macro_rules! impl_standby_shutdown {
   (Max5533) => {
     impl_standby_shutdown!(@inner Max5533, Standby);
@@ -219,7 +109,105 @@ macro_rules! impl_standby_shutdown {
   }
 }
 
-impl_standby_shutdown!(Max5532);
-impl_standby_shutdown!(Max5533);
-impl_standby_shutdown!(Max5534);
-impl_standby_shutdown!(Max5535);
+macro_rules! impl_max {
+  ($(#[$attr:meta]),* $max_ty:ident) => {
+    $(
+      #[$attr]
+    )*
+    #[derive(Debug)]
+    pub struct $max_ty<W, MODE> {
+      writer: W,
+      _mode: PhantomData<MODE>,
+    }
+
+    impl<W, MODE> $max_ty<W, MODE> {
+      /// Create a new DAC with the given writer.
+      pub fn new(writer: W) -> $max_ty<W, Shutdown> {
+        $max_ty { writer, _mode: PhantomData }
+      }
+
+      /// Release the contained writer.
+      pub fn release(self) -> W {
+        self.writer
+      }
+    }
+
+    impl<W, MODE> $max_ty<W, MODE>
+    where
+      W: Write<u8>
+    {
+      /// Load input register A from shift register.
+      #[inline]
+      pub fn input_a(&mut self, value: u16) -> Result<(), W::Error> {
+        self.writer.write(&command_bytes(0b0001, value))
+      }
+
+      /// Load input register B from shift register.
+      #[inline]
+      pub fn input_b(&mut self, value: u16) -> Result<(), W::Error> {
+        self.writer.write(&command_bytes(0b0010, value))
+      }
+
+      impl_into_mode!("normal operation", $max_ty, Normal,   into_normal,   0b1101);
+      impl_into_mode!("shutdown",         $max_ty, Shutdown, into_shutdown, 0b1110);
+    }
+    
+    impl<W> $max_ty<W, Normal>
+    where
+      W: Write<u8>
+    {
+      /// Load DAC registers A and B from respective input registers
+      /// and update respective DAC outputs.
+      #[inline]
+      pub fn dac_ab(&mut self, value: u16) -> Result<(), W::Error> {
+        self.writer.write(&command_bytes(0b1000, value))
+      }
+
+      /// Load input and DAC register A from shift register A and
+      /// load DAC register B from input register B.
+      pub fn input_a_dac_ab(&mut self, value: u16) -> Result<(), W::Error> {
+        self.writer.write(&command_bytes(0b1001, value))
+      }
+
+      /// Load input and DAC register B from shift register B and
+      /// load DAC register A from input register A.
+      pub fn input_b_dac_ab(&mut self, value: u16) -> Result<(), W::Error> {
+        self.writer.write(&command_bytes(0b1010, value))
+      }
+
+      impl_into_mode!("standby", $max_ty, Standby, into_standby, 0b1100);
+    }
+    
+    impl_standby_shutdown!($max_ty);
+  }
+}
+
+impl_max!(
+  /// Struct representing a MAX5532 DAC.
+  Max5532
+);
+impl_max!(
+  /// Struct representing a MAX5533 DAC.
+  Max5533
+);
+impl_max!(
+  /// Struct representing a MAX5534 DAC.
+  Max5534
+);
+impl_max!(
+  /// Struct representing a MAX5535 DAC.
+  Max5535
+);
+
+/// Internal voltage reference for MAX5533/MAX5535.
+#[derive(Debug, Clone, Copy)]
+pub enum Vref {
+  /// 1.214 V
+  M1214 = 0b00,
+  /// 1.940 V
+  M1940 = 0b01,
+  /// 2.425 V
+  M2425 = 0b10,
+  /// 3.885 V
+  M3885 = 0b11,
+}
