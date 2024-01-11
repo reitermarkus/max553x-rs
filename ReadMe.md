58ce20e1ed1721fea8f6627273306ff283b380ee
Markus Reiter <me@reitermark.us>
2024-01-11 16:31:53 +0100

    Update to `embedded-hal` 1.0.

--- ReadMe.md
+++ ReadMe.md
@@ -9,5 +9,5 @@ Add this to your `Cargo.toml`:
 
 ```toml
 [dependencies]
-max553x = "0.2"
+max553x = "0.3"
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 
 use core::marker::PhantomData;
 
-use embedded_hal::blocking::spi::Write;
+use embedded_hal::spi::SpiDevice;
 
 /// Marks a DAC in normal operation mode.
 pub enum Normal {}
@@ -21,17 +21,11 @@ const fn command_bytes(control_bits: u8, mut data_bits: u16) -> [u8; 2] {
     data_bits = 0b1111_1111_1111
   }
 
-  [
-    (control_bits << 4) | ((data_bits >> 8) as u8 & 0xf),
-    data_bits as u8,
-  ]
+  [(control_bits << 4) | ((data_bits >> 8) as u8 & 0xf), data_bits as u8]
 }
 
 const fn vref_command_bytes(control_bits: u8, vref: Vref) -> [u8; 2] {
-  [
-    (control_bits << 4) | ((vref as u8) << 2),
-    0
-  ]
+  [(control_bits << 4) | ((vref as u8) << 2), 0]
 }
 
 macro_rules! impl_into_mode {
@@ -51,18 +45,18 @@ macro_rules! impl_into_mode {
     /// Enter
     #[doc = $desc]
     /// mode.
-    pub fn $fn_name(mut self) -> Result<$max_ty<W, $mode_ty>, W::Error> {
-      self.writer.write(&command_bytes($control_bits, 0))?;
-      Ok($max_ty { writer: self.writer, _mode: PhantomData })
+    pub fn $fn_name(mut self) -> Result<$max_ty<SPI, $mode_ty>, SPI::Error> {
+      self.spi.write(&command_bytes($control_bits, 0))?;
+      Ok($max_ty { spi: self.spi, _mode: PhantomData })
     }
   };
   (@with_vref $desc:expr, $max_ty:ident, $mode_ty:ident, $fn_name:ident, $control_bits:expr) => {
     /// Enter
     #[doc = $desc]
     /// mode and set the internal voltage reference.
-    pub fn $fn_name(mut self, vref: Vref) -> Result<$max_ty<W, $mode_ty>, W::Error> {
-      self.writer.write(&vref_command_bytes($control_bits, vref))?;
-      Ok($max_ty { writer: self.writer, _mode: PhantomData })
+    pub fn $fn_name(mut self, vref: Vref) -> Result<$max_ty<SPI, $mode_ty>, SPI::Error> {
+      self.spi.write(&vref_command_bytes($control_bits, vref))?;
+      Ok($max_ty { spi: self.spi, _mode: PhantomData })
     }
   };
 }
@@ -116,15 +110,15 @@ macro_rules! impl_standby_shutdown {
     impl_standby_shutdown!(@inner $max_ty, Shutdown);
   };
   (@inner $max_ty:ident, $mode_ty:ident) => {
-    impl<W> $max_ty<W, $mode_ty>
+    impl<SPI> $max_ty<SPI, $mode_ty>
     where
-      W: Write<u8>
+      SPI: SpiDevice<u8>
     {
       /// Load DAC registers A and B from respective input registers, update
       /// respective DAC outputs and enter normal operation mode.
       #[inline]
-      pub fn dac_ab(self) -> Result<$max_ty<W, Normal>, W::Error> {
-        let mut max_553x: $max_ty<W, Normal> = $max_ty { writer: self.writer, _mode: PhantomData };
+      pub fn dac_ab(self) -> Result<$max_ty<SPI, Normal>, SPI::Error> {
+        let mut max_553x: $max_ty<SPI, Normal> = $max_ty { spi: self.spi, _mode: PhantomData };
         max_553x.dac_ab()?;
         Ok(max_553x)
       }
@@ -132,8 +126,8 @@ macro_rules! impl_standby_shutdown {
       /// Load input and DAC register A from shift register A,
       /// load DAC register B from input register B
       /// and enter normal operation mode.
-      pub fn input_a_dac_ab(self, value: u16) -> Result<$max_ty<W, Normal>, W::Error> {
-        let mut max_553x: $max_ty<W, Normal> = $max_ty { writer: self.writer, _mode: PhantomData };
+      pub fn input_a_dac_ab(self, value: u16) -> Result<$max_ty<SPI, Normal>, SPI::Error> {
+        let mut max_553x: $max_ty<SPI, Normal> = $max_ty { spi: self.spi, _mode: PhantomData };
         max_553x.input_a_dac_ab(value)?;
         Ok(max_553x)
       }
@@ -141,16 +135,16 @@ macro_rules! impl_standby_shutdown {
       /// Load input and DAC register B from shift register A,
       /// load DAC register A from input register A
       /// and enter normal operation mode.
-      pub fn input_b_dac_ab(self, value: u16) -> Result<$max_ty<W, Normal>, W::Error> {
-        let mut max_553x: $max_ty<W, Normal> = $max_ty { writer: self.writer, _mode: PhantomData };
+      pub fn input_b_dac_ab(self, value: u16) -> Result<$max_ty<SPI, Normal>, SPI::Error> {
+        let mut max_553x: $max_ty<SPI, Normal> = $max_ty { spi: self.spi, _mode: PhantomData };
         max_553x.input_b_dac_ab(value)?;
         Ok(max_553x)
       }
 
       /// Load input registers A and B and DAC registers A and B from shift register
       /// and enter normal operation mode.
-      pub fn input_ab_dac_ab(self, value: u16) -> Result<$max_ty<W, Normal>, W::Error> {
-        let mut max_553x: $max_ty<W, Normal> = $max_ty { writer: self.writer, _mode: PhantomData };
+      pub fn input_ab_dac_ab(self, value: u16) -> Result<$max_ty<SPI, Normal>, SPI::Error> {
+        let mut max_553x: $max_ty<SPI, Normal> = $max_ty { spi: self.spi, _mode: PhantomData };
         max_553x.input_ab_dac_ab(value)?;
         Ok(max_553x)
       }
@@ -166,18 +160,32 @@ macro_rules! impl_max {
     /// # Usage
     ///
     /// ```rust
-    /// # fn main() -> Result<(), embedded_hal_mock::MockError> {
-    /// # use embedded_hal_mock::{spi::{Mock as SpiMock, Transaction as SpiTransaction}, delay::MockNoop as Delay};
+    /// # fn main() -> Result<(), embedded_hal::spi::ErrorKind> {
+    /// # use embedded_hal_mock::eh1::{spi::{Mock as SpiMock, Transaction as SpiTransaction}};
     #[doc = concat!("use ", doc_imports!($max_ty), ";")]
     /// #
-    /// # let spi = SpiMock::new(&[
-    #[doc = concat!("#   SpiTransaction::write(vec![0b1101_0000 | ", doc_vref_value!($max_ty), ", 0b00000000]), // Into normal mode.")]
-    /// #   SpiTransaction::write(vec![0b0001_0100, 0b11010010]), // Load input register A with value 1234.
-    /// #   SpiTransaction::write(vec![0b0010_1111, 0b11111111]), // Load input register B with value 4095.
-    /// #   SpiTransaction::write(vec![0b1000_0000, 0b00000000]), // Load DAC registers from input registers.
-    /// #   SpiTransaction::write(vec![0b1001_0100, 0b11010010]), // Load input register A with value 1234 and load DAC register A.
-    /// #   SpiTransaction::write(vec![0b1010_1111, 0b11111111]), // Load input register B with value 4095 and load DAC register B.
-    #[doc = concat!("#   SpiTransaction::write(vec![0b1110_0000 | ", doc_vref_value!($max_ty), ", 0b00000000]), // Into shutdown mode.")]
+    /// # let mut spi = SpiMock::new(&[
+    /// #   SpiTransaction::transaction_start(),
+    #[doc = concat!("#   SpiTransaction::write_vec(vec![0b1101_0000 | ", doc_vref_value!($max_ty), ", 0b00000000]), // Into normal mode.")]
+    /// #   SpiTransaction::transaction_end(),
+    /// #   SpiTransaction::transaction_start(),
+    /// #   SpiTransaction::write_vec(vec![0b0001_0100, 0b11010010]), // Load input register A with value 1234.
+    /// #   SpiTransaction::transaction_end(),
+    /// #   SpiTransaction::transaction_start(),
+    /// #   SpiTransaction::write_vec(vec![0b0010_1111, 0b11111111]), // Load input register B with value 4095.
+    /// #   SpiTransaction::transaction_end(),
+    /// #   SpiTransaction::transaction_start(),
+    /// #   SpiTransaction::write_vec(vec![0b1000_0000, 0b00000000]), // Load DAC registers from input registers.
+    /// #   SpiTransaction::transaction_end(),
+    /// #   SpiTransaction::transaction_start(),
+    /// #   SpiTransaction::write_vec(vec![0b1001_0100, 0b11010010]), // Load input register A with value 1234 and load DAC register A.
+    /// #   SpiTransaction::transaction_end(),
+    /// #   SpiTransaction::transaction_start(),
+    /// #   SpiTransaction::write_vec(vec![0b1010_1111, 0b11111111]), // Load input register B with value 4095 and load DAC register B.
+    /// #   SpiTransaction::transaction_end(),
+    /// #   SpiTransaction::transaction_start(),
+    #[doc = concat!("#   SpiTransaction::write_vec(vec![0b1110_0000 | ", doc_vref_value!($max_ty), ", 0b00000000]), // Into shutdown mode.")]
+    /// #   SpiTransaction::transaction_end(),
     /// # ]);
     ///
     #[doc = concat!("let dac = ", stringify!($max_ty), "::new(spi);")]
@@ -201,75 +209,80 @@ macro_rules! impl_max {
     /// dac.input_b_dac_ab(4095)?;
     ///
     /// // Turn off.
-    #[doc = concat!("dac.into_shutdown(", doc_vref!($max_ty), ")?;")]
-    /// #
+    #[doc = concat!("let dac = dac.into_shutdown(", doc_vref!($max_ty), ")?;")]
+    ///
+    /// // Release SPI peripheral again.
+    /// let spi = dac.release();
+    /// # let mut spi = spi;
+    /// # spi.done();
+    /// drop(spi);
     /// # Ok(())
     /// # }
     /// ```
     #[derive(Debug)]
-    pub struct $max_ty<W, MODE> {
-      writer: W,
+    pub struct $max_ty<SPI, MODE> {
+      spi: SPI,
       _mode: PhantomData<MODE>,
     }
 
-    impl<W> $max_ty<W, Shutdown> {
-      /// Create a new DAC with the given writer.
-      pub const fn new(writer: W) -> $max_ty<W, Shutdown> {
-        $max_ty { writer, _mode: PhantomData }
+    impl<SPI> $max_ty<SPI, Shutdown> {
+      /// Create a new DAC with the given spi.
+      pub const fn new(spi: SPI) -> $max_ty<SPI, Shutdown> {
+        $max_ty { spi, _mode: PhantomData }
       }
 
-      /// Release the contained writer.
-      pub fn release(self) -> W {
-        self.writer
+      /// Release the contained spi.
+      pub fn release(self) -> SPI {
+        self.spi
       }
     }
 
-    impl<W, MODE> $max_ty<W, MODE>
+    impl<SPI, MODE> $max_ty<SPI, MODE>
     where
-      W: Write<u8>
+      SPI: SpiDevice<u8>
     {
       /// Load input register A from shift register.
       #[inline]
-      pub fn input_a(&mut self, value: u16) -> Result<(), W::Error> {
-        self.writer.write(&command_bytes(0b0001, value))
+      pub fn input_a(&mut self, value: u16) -> Result<(), SPI::Error> {
+        self.spi.write(&command_bytes(0b0001, value))
       }
 
       /// Load input register B from shift register.
       #[inline]
-      pub fn input_b(&mut self, value: u16) -> Result<(), W::Error> {
-        self.writer.write(&command_bytes(0b0010, value))
+      pub fn input_b(&mut self, value: u16) -> Result<(), SPI::Error> {
+        self.spi.write(&command_bytes(0b0010, value))
       }
 
       impl_into_mode!("normal operation", $max_ty, Normal,   into_normal,   0b1101);
       impl_into_mode!("shutdown",         $max_ty, Shutdown, into_shutdown, 0b1110);
     }
 
-    impl<W> $max_ty<W, Normal>
+    impl<SPI> $max_ty<SPI, Normal>
     where
-      W: Write<u8>
+      SPI: SpiDevice<u8>
     {
       /// Load DAC registers A and B from respective input registers
       /// and update respective DAC outputs.
       #[inline]
-      pub fn dac_ab(&mut self) -> Result<(), W::Error> {
-        self.writer.write(&command_bytes(0b1000, 0))
+      pub fn dac_ab(&mut self) -> Result<(), SPI::Error> {
+        self.spi.write(&command_bytes(0b1000, 0))
       }
 
       /// Load input and DAC register A from shift register A and
       /// load DAC register B from input register B.
-      pub fn input_a_dac_ab(&mut self, value: u16) -> Result<(), W::Error> {
-        self.writer.write(&command_bytes(0b1001, value))
+      pub fn input_a_dac_ab(&mut self, value: u16) -> Result<(), SPI::Error> {
+        self.spi.write(&command_bytes(0b1001, value))
       }
 
       /// Load input and DAC register B from shift register B and
       /// load DAC register A from input register A.
-      pub fn input_b_dac_ab(&mut self, value: u16) -> Result<(), W::Error> {
-        self.writer.write(&command_bytes(0b1010, value))
+      pub fn input_b_dac_ab(&mut self, value: u16) -> Result<(), SPI::Error> {
+        self.spi.write(&command_bytes(0b1010, value))
       }
 
       /// Load input registers A and B and DAC registers A and B from shift register.
-      pub fn input_ab_dac_ab(&mut self, value: u16) -> Result<(), W::Error> {
-        self.writer.write(&command_bytes(0b1111, value))
+      pub fn input_ab_dac_ab(&mut self, value: u16) -> Result<(), SPI::Error> {
+        self.spi.write(&command_bytes(0b1111, value))
       }
 
       impl_into_mode!("standby", $max_ty, Standby, into_standby, 0b1100);
